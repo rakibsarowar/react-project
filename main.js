499fb2746caf75389b95a77eb2c300d0d58e30dc
rakibsarowar <rakibsarowar@gmail.com>
2023-11-13 22:50:14 +0600

    mounted vue app

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'socks',
+            // solution
+            description: "A warm fuzzy of socks."
+            // solution
+        }
+    }
+})
